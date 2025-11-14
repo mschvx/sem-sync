cc1910407daf5fba43fde971d7146281d2959dcf
omracc <jeantonio@up.edu.ph>
2025-11-14 18:07:09 +0800

    Update Register.java
    added logic for registration, errors for blanks, special characters, and existing accounts

--- src/application/Register.java
+++ src/application/Register.java
@@ -10,16 +10,72 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Font;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javafx.animation.ScaleTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.util.Duration;
 
 public class Register {	
+	
+	Path path = Path.of(System.getProperty("user.dir"));
+	
+	// used for validating if user created does not already exist
+	private ObservableList<User> loadData(Path path) {
+        ObservableList<User> list = FXCollections.<User>observableArrayList();
+        Path folder = Paths.get("placeholder");
+        Path file = folder.resolve("placeholder.csv");
+
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+
+        if (!Files.exists(file)) {
+            errorAlert.setHeaderText("Error!");
+            errorAlert.setContentText("File not Found!");
+            errorAlert.showAndWait();
+            return list;
+        }
+
+        try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8)) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] parts = line.split(",");
+
+                String username = parts[0].trim();
+                String password = parts[1].trim();
+
+                list.add(new User(username, password));
+            }
+
+        } catch (IOException e) {
+            errorAlert.setHeaderText("Error");
+            errorAlert.setContentText("Cannot Load.");
+            errorAlert.showAndWait();
+            return list;
+        }
+
+        return list;
+    }
 
     public void showRegister(Stage primaryStage) {
         Pane root = new Pane();
@@ -163,7 +219,66 @@ public class Register {
                 "-fx-text-fill: black;" +
                 "-fx-border-width: 4;");
         root.getChildren().add(passwordField);
-
+        
+     // Loading data from CSV File
+        ObservableList<User> data = loadData(path);
+        
+        // logic for creating account
+        createButton.setOnAction(e -> {
+        	String uname = usernameField.getText().trim();
+        	String pass = passwordField.getText().trim();
+        	ToggleButton selectedDegree = (ToggleButton) degreeGroup.getSelectedToggle();
+        	boolean usernameFound = false;
+        	
+        	Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        	
+        	for (User user : data) {
+        		if (user.getUsername().equals(uname)) {
+        			usernameFound = true;
+        			break;
+        		}
+        	}
+        	// user already exists
+        	if (usernameFound = true) {
+        		errorAlert.setHeaderText("Invalid Username");
+        		errorAlert.setContentText("This username is already taken. Please choose another one.");
+        		errorAlert.showAndWait();
+        	}
+        	// blank inputs
+        	else if (uname.isBlank() || pass.isEmpty()) {
+        		errorAlert.setHeaderText("Missing Fields!");
+        		errorAlert.setContentText("Please fill in both username and password.");
+        		errorAlert.showAndWait();
+        	}
+        	else if (selectedDegree == null) {
+        		errorAlert.setHeaderText("Missing Degree Program!");
+        		errorAlert.setContentText("Please select a degree program.");
+        		errorAlert.showAndWait();
+        	}
+        	// special characters
+        	else if (!isAlphanumeric(uname)) {
+        		errorAlert.setHeaderText("Invalid Username");
+        		errorAlert.setContentText("Username can only contain letters and numbers.\nNo spaces or special characters allowed.");
+        		errorAlert.showAndWait();
+        	}
+        	else if (!isAlphanumeric(pass)) {
+        		errorAlert.setHeaderText("Invalid Paddword");
+        		errorAlert.setContentText("Password can only contain letters and numbers.\\nNo spaces or special characters allowed.");
+        		errorAlert.showAndWait();
+        	}
+        	else {
+        		Alert success = new Alert(Alert.AlertType.INFORMATION);
+        		success.setTitle("Account Created");
+        		success.setHeaderText(null);
+        		success.setContentText("Account created. Now go log in!");
+        		success.showAndWait();
+        		
+        		// go back to log in page
+        		Main main = new Main();
+        		main.start(primaryStage);
+        	}
+        });
+       
 
         // Set up window properties
         primaryStage.setScene(scene);
@@ -173,5 +288,11 @@ public class Register {
         primaryStage.setWidth(visualBounds.getWidth());
         primaryStage.setHeight(visualBounds.getHeight());
         primaryStage.show();
+    }
+    
+    
+    // method for detecting special character inputs
+    private boolean isAlphanumeric(String text) {
+    	return text.matches("^[A-Za-z0-9]+$");
     }
 }
