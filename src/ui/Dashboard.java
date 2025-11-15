d6e3402de013cb9fd4e5ce18aefb4a23cbc610d8
lee castro <castroallanelee@gmail.com>
2025-11-16 01:50:29 +0800

    added dashboard format

--- src/ui/Dashboard.java
+++ src/ui/Dashboard.java
@@ -12,6 +12,11 @@ import users.DegreeLookup;
 import users.DegreeProgram;
 import users.User;
 import javafx.scene.text.Font;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 public class Dashboard {
 
@@ -42,6 +47,46 @@ public class Dashboard {
         greetLabel.setLayoutX(60);
         greetLabel.setLayoutY(60);
         root.getChildren().add(greetLabel);
+
+        // -- Dropdown --
+        ComboBox<String> dropdown = new ComboBox<>();
+        dropdown.getItems().addAll("From","dataset","add kau");
+        dropdown.setPromptText("Select course:");
+        dropdown.setLayoutX(220);
+        dropdown.setLayoutY(180);
+        dropdown.setPrefWidth(400);
+        dropdown.setStyle("-fx-font-size: 18px;");
+
+        // -- Buttons --
+        Button btnAdd = new Button("Add");
+        btnAdd.setLayoutX(220);
+        btnAdd.setLayoutY(260);
+        btnAdd.setPrefWidth(120);
+        btnAdd.setFont(Fonts.loadSensaWild(24));
+
+        Button btnDelete = new Button("Delete");
+        btnDelete.setLayoutX(360);
+        btnDelete.setLayoutY(260);
+        btnDelete.setPrefWidth(120);
+        btnDelete.setFont(Fonts.loadSensaWild(24));
+
+
+        // -- List of items added by user--
+        Label itemsLabel = new Label("Your Items");
+        itemsLabel.setFont(Font.font("Montserrat", 28));
+        itemsLabel.setLayoutX(220);
+        itemsLabel.setLayoutY(330);
+
+        ObservableList<String> listItems = FXCollections.observableArrayList();
+        listItems.add("None");
+        ListView<String> listView = new ListView<>(listItems);
+        listView.setLayoutX(220);
+        listView.setLayoutY(370);
+        listView.setPrefWidth(1000);
+        listView.setPrefHeight(420);
+
+        // Add to root
+        root.getChildren().addAll(dropdown, btnAdd, btnDelete, itemsLabel, listView);
 
         primaryStage.show();
     }
